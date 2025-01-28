ad3a13dc73daacec6dc66052e674cdc33afd8245
Default <guilianolehmann@live.de>
2025-01-28 23:17:23 +0100

    Update test_catalyst and test_load_pqc

--- test_catalyst.py
+++ test_catalyst.py
@@ -1,24 +1,27 @@
-"""Try out all possible combinations of alternative signature data and verify the signature of a catalyst certificate."""
+"""Try out all possible combinations of alternative signature data and verify the signature of a catalyst cert."""
+
 import glob
-import pprint
 from itertools import product
 
-from cryptography.exceptions import InvalidSignature
-
+import oqs
 from pq_logic.fips.fips204 import ML_DSA
-from pq_logic.hybrid_sig.catalyst_logic import verify_catalyst_signature, verify_catalyst_signature_migrated, \
-    validate_catalyst_extension, prepare_alt_signature_data
-from pq_logic.keys.abstract_pq import PQPublicKey, PQPrivateKey
+from pq_logic.hybrid_sig.catalyst_logic import (
+    prepare_alt_signature_data,
+    validate_catalyst_extension,
+)
+from pq_logic.keys.abstract_pq import PQPrivateKey, PQPublicKey
 from pq_logic.keys.sig_keys import MLDSAPublicKey
+from pyasn1_alt_modules import rfc9480
 from resources.certutils import parse_certificate
 from resources.oid_mapping import KEY_CLASS_MAPPING, may_return_oid_to_name
-from unit_tests.utils_for_test import print_chain_subject_and_issuer, get_subject_and_issuer
-from pyasn1_alt_modules import rfc5280, rfc9480
-
-import oqs
+from unit_tests.utils_for_test import get_subject_and_issuer
 
 
 def get_catalyst_certs() -> list[str]:
+    """Get all catalyst certificates from the specified directory.
+
+    :return: The list of catalyst certificates.
+    """
     pem_files = []
     for file in glob.iglob("./data/pqc-certificates/providers/**", recursive=True):
         if file.endswith(".der") and "catalyst" in file:
@@ -29,37 +32,42 @@ def get_catalyst_certs() -> list[str]:
     return pem_files
 
 
-
 def get_key_name(key) -> str:
+    """Get the name of the key.
 
+    :param key: The key to get the name of.
+    :return: The name of the key.
+    """
     if isinstance(key, (PQPublicKey, PQPrivateKey)):
-        return key.key_name
+        return key.name
     else:
         return str(KEY_CLASS_MAPPING.get(type(key).__name__, key))
 
 
 def log_cert_infos(asn1cert: rfc9480.CMPCertificate):
+    """Log the information of the certificate.
+
+    :param asn1cert: The certificate to be logged.
+    :return: The public key, the name of the public key algorithm, and the signature.
+    """
     tmp = get_subject_and_issuer(asn1cert)
-    tmp += "\nSignature algorithm: " + may_return_oid_to_name(asn1cert['tbsCertificate']['signature']['algorithm'])
-    tmp += "\nPublic key algorithm: " + may_return_oid_to_name(asn1cert['tbsCertificate']['subjectPublicKeyInfo']['algorithm']['algorithm'])
+    tmp += "\nSignature algorithm: " + may_return_oid_to_name(asn1cert["tbsCertificate"]["signature"]["algorithm"])
+    tmp += "\nPublic key algorithm: " + may_return_oid_to_name(
+        asn1cert["tbsCertificate"]["subjectPublicKeyInfo"]["algorithm"]["algorithm"]
+    )
     tmp += "\nCatalyst Extension: \n"
     extension = validate_catalyst_extension(asn1cert)
-    tmp += "Catalyst AltPubKey: " + may_return_oid_to_name(extension['spki']["algorithm"]["algorithm"])
-    tmp += "\nCatalyst AltSigAlg: " + may_return_oid_to_name(extension['alg_id']["algorithm"])
+    tmp += "Catalyst AltPubKey: " + may_return_oid_to_name(extension["spki"]["algorithm"]["algorithm"])
+    tmp += "\nCatalyst AltSigAlg: " + may_return_oid_to_name(extension["alg_id"]["algorithm"])
 
-    pub_key = extension['spki']["subjectPublicKey"].asOctets()
+    pub_key = extension["spki"]["subjectPublicKey"].asOctets()
 
-
-    name = may_return_oid_to_name(extension['spki']["algorithm"]["algorithm"])
-    #print(tmp)
+    name = may_return_oid_to_name(extension["spki"]["algorithm"]["algorithm"])
+    # print(tmp)
     return pub_key, name, extension["signature"]
 
 
-
-
-
-def _try2(asn1cert: rfc9480.CMPCertificate,
-          pub_key: bytes, name: str, signature: bytes) -> bool:
+def _try2(asn1cert: rfc9480.CMPCertificate, pub_key: bytes, name: str, signature: bytes) -> bool:
     """Try all possible combinations of alternative signature data and verify the signature.
 
     :param asn1cert: The certificate to be verified.
@@ -68,55 +76,51 @@ def _try2(asn1cert: rfc9480.CMPCertificate,
     :param signature: The signature to be verified.
     :return: Whether the verification was successful.
     """
-
     # verify the key size.
     MLDSAPublicKey.from_public_bytes(pub_key, name)
 
-    for exclude_alt_extensions, only_tbs_cert, exclude_signature_field, exclude_spki \
-            in product([True, False], repeat=4):
+    for exclude_alt_extensions, only_tbs_cert, exclude_signature_field, exclude_spki in product(
+        [True, False], repeat=4
+    ):
         # Prepare alternative signature data with the current combination
         alt_sig_data = prepare_alt_signature_data(
             cert=asn1cert,
             exclude_alt_extensions=exclude_alt_extensions,
             only_tbs_cert=only_tbs_cert,
-            exclude_signature_field=exclude_signature_field, #means the signature field inside tbsCertificate,
+            exclude_signature_field=exclude_signature_field,  # means the signature field inside tbsCertificate,
             # which is the signature algorithm.
             exclude_first_spki=exclude_spki,
         )
 
-
-
-        out = ML_DSA(name).verify(pk=pub_key,
-                                  m=alt_sig_data,
-                                  sig=signature,
-                                  ctx=b"")
-
+        out = ML_DSA(name).verify(pk=pub_key, m=alt_sig_data, sig=signature, ctx=b"")
 
         for sigalg in oqs.get_enabled_sig_mechanisms():
             if sigalg.startswith("Dilithium") or sigalg.startswith("ML-D"):
                 with oqs.Signature(sigalg) as verifier:
                     try:
-                         is_valid = verifier.verify(alt_sig_data, signature, pub_key)
+                        is_valid = verifier.verify(alt_sig_data, signature, pub_key)
 
-                         if is_valid:
-                           print(f"Verification successful with {sigalg} with: "
-                                   f"exclude_alt_extensions={exclude_alt_extensions}, "
-                                   f"only_tbs_cert={only_tbs_cert}, "
-                                   f"exclude_signature_field={exclude_signature_field}"
-                                   f"exclude_spki={exclude_spki}")
-                           return True
-                    except Exception as e:
-                        #print(f"Verification failed for {sigalg}:", e)
+                        if is_valid:
+                            print(
+                                f"Verification successful with {sigalg} with: "
+                                f"exclude_alt_extensions={exclude_alt_extensions}, "
+                                f"only_tbs_cert={only_tbs_cert}, "
+                                f"exclude_signature_field={exclude_signature_field}"
+                                f"exclude_spki={exclude_spki}"
+                            )
+                            return True
+                    except Exception:
+                        # print(f"Verification failed for {sigalg}:", e)
                         continue
 
-
-
         if out:
-            print(f"Verification successful with {name} with: "
-                  f"exclude_alt_extensions={exclude_alt_extensions}, "
-                  f"only_tbs_cert={only_tbs_cert}, "
-                  f"exclude_signature_field={exclude_signature_field}"
-                  f"exclude_spki={exclude_spki}")
+            print(
+                f"Verification successful with {name} with: "
+                f"exclude_alt_extensions={exclude_alt_extensions}, "
+                f"only_tbs_cert={only_tbs_cert}, "
+                f"exclude_signature_field={exclude_signature_field}"
+                f"exclude_spki={exclude_spki}"
+            )
             return True
 
     print("Verification failed for all options for ", name)
@@ -126,7 +130,7 @@ def _try2(asn1cert: rfc9480.CMPCertificate,
 pem_files = get_catalyst_certs()
 
 for pem_file in pem_files:
-    with open(pem_file, 'rb') as file:
+    with open(pem_file, "rb") as file:
         cert = parse_certificate(file.read())
         pub_key, name, signature = log_cert_infos(cert)
         _try2(cert, pub_key, name, signature)

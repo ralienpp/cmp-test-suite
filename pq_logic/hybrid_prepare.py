6f65aa36f7dd1ed09c7dab755536166f6060eef8
Default <guilianolehmann@live.de>
2025-02-07 17:43:30 +0100

    Update hybrid_prepare.py

--- pq_logic/hybrid_prepare.py
+++ pq_logic/hybrid_prepare.py
@@ -4,4 +4,8 @@
 # pylint: disable=too-few-public-methods
 """Import all functions from hybrid_sig to ensure easy access for the Robot Framework."""
 
-from hybrid_sig import *  # noqa: F401,F403
+from pq_logic.hybrid_sig.catalyst_logic import *  # noqa: F401,F403
+from pq_logic.hybrid_sig.chameleon_logic import *  # noqa: F401,F403
+from pq_logic.hybrid_sig.sun_lamps_hybrid_scheme_00 import *  # noqa: F401,F403
+from pq_logic.hybrid_sig.certdiscovery import *  # noqa: F401,F403
+from pq_logic.hybrid_sig.cert_binding_for_multi_auth import *  # noqa: F401,F403

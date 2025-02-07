7e82a13ec98ac945e07e677127d08f22baf84e98
Default <guilianolehmann@live.de>
2025-02-07 15:37:10 +0100

    Add hybrid_prepare.py

---
+++ pq_logic/hybrid_prepare.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright 2024 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+# pylint: disable=too-few-public-methods
+"""Import all functions from hybrid_sig to ensure easy access for the Robot Framework."""
+
+from hybrid_sig import *  # noqa: F401,F403

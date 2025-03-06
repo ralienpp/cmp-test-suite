216e60f8cea7cae87ca6e04d422a30cae3320958
Default <guilianolehmann@live.de>
2025-03-07 00:42:22 +0100

    Add tests TestGenerateKeyByName

---
+++ unit_tests/tests_keys_related/test_generate_key_by_name.py
@@ -0,0 +1,57 @@
+import unittest
+
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from pq_logic.combined_factory import CombinedKeyFactory
+
+
+class TestGenerateKeyByName(unittest.TestCase):
+    def test_generate_key_by_name_trad(self):
+        """
+        GIVEN the key name="rsa".
+        WHEN the key is generated,
+        THEN the key is an instance of RSAPrivateKey
+        """
+        algorithm = "rsa"
+        key = CombinedKeyFactory.generate_key_from_name(algorithm)
+        self.assertIsInstance(key, RSAPrivateKey)
+
+    def test_generate_key_by_name_chempat(self):
+        """
+        GIVEN the key name="chempat-ml-kem-768-ecdh-secp256r1".
+        WHEN the key is generated,
+        THEN the key is correctly generated.
+        """
+        algorithm = "chempat-ml-kem-768-ecdh-secp256r1"
+        key = CombinedKeyFactory.generate_key_from_name(algorithm)
+        self.assertEqual(key.name, algorithm)
+
+    def test_generate_key_by_name_composite_sig(self):
+        """
+        GIVEN the key name="composite-sig-ml-dsa-44-rsa2048".
+        WHEN the key is generated,
+        THEN the key is correctly generated.
+        """
+        algorithm = "composite-sig-ml-dsa-44-rsa2048"
+        key = CombinedKeyFactory.generate_key_from_name(algorithm)
+        self.assertEqual(key.name, algorithm)
+
+    def test_generate_key_by_name_composite_kem(self):
+        """
+        GIVEN the key name="composite-kem-ml-kem-768-ecdh-secp384r1".
+        WHEN the key is generated,
+        THEN the key is correctly generated.
+        """
+        algorithm = "composite-kem-ml-kem-768-ecdh-secp384r1"
+        key = CombinedKeyFactory.generate_key_from_name(algorithm)
+        self.assertEqual(key.name, algorithm)
+
+    def test_generate_key_by_name_invalid_algorithm(self):
+        """
+        GIVEN the key name="composite-kem-ml-kem-768-ecdh-secp256r1".
+        WHEN the key is generated,
+        THEN the InvalidKeyCombination exception is raised,
+        because this combination is not allowed.
+        """
+        algorithm = "composite-kem-ml-kem-768-ecdh-secp256r1"
+        with self.assertRaises(ValueError):
+            CombinedKeyFactory.generate_key_from_name(algorithm)

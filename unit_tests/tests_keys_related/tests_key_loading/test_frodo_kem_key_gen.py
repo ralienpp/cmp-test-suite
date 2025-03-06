8c884437dfbca4ad1fe5d6bbac11f2443805c677
Default <guilianolehmann@live.de>
2025-03-07 00:43:20 +0100

    Add load keys test cases

---
+++ unit_tests/tests_keys_related/tests_key_loading/test_frodo_kem_key_gen.py
@@ -0,0 +1,41 @@
+import unittest
+
+from cryptography.hazmat.primitives import serialization
+from pyasn1.codec.der import decoder
+from pyasn1_alt_modules import rfc5958
+from pq_logic.keys.key_pyasn1_utils import parse_key_from_one_asym_key
+from pq_logic.keys.kem_keys import FrodoKEMPrivateKey, FrodoKEMPublicKey
+
+class TestFrodoKEMKeyGen(unittest.TestCase):
+
+
+    def test_load_from_seed_asym_one(self):
+        """
+        GIVEN a seed and a `OneAsymmetricKey` structure.
+        WHEN a key is generated from the seed and loaded from the `OneAsymmetricKey` structure.
+        THEN the key should be generated successfully.
+        """
+        private_key = FrodoKEMPrivateKey(alg_name="frodokem-640-aes")
+
+        private_bytes = private_key.private_bytes(
+            encoding=serialization.Encoding.DER,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+
+        one_asym_key, rest = decoder.decode(private_bytes, asn1Spec=rfc5958.OneAsymmetricKey())
+        self.assertEqual(rest, b"")
+
+        priv_key2 = FrodoKEMPrivateKey.from_private_bytes(
+            data=one_asym_key["privateKey"].asOctets(),
+            name="frodokem-640-aes",
+        )
+
+        out = one_asym_key["publicKey"].asOctets()
+        self.assertEqual(out, private_key.public_key().public_bytes_raw())
+        pub_key = FrodoKEMPublicKey.from_public_bytes(data=out, name="frodokem-640-aes")
+        self.assertEqual(pub_key.public_bytes_raw(), private_key.public_key().public_bytes_raw())
+
+        key2 = parse_key_from_one_asym_key(private_bytes)
+        self.assertEqual(key2.private_bytes_raw(), private_key.private_bytes_raw())
+        self.assertEqual(key2.public_key().public_bytes_raw(), private_key.public_key().public_bytes_raw())
